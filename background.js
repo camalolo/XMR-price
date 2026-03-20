2ec80788581df40951e8f6f4421f25c974667a57
Laurent Camarasa <camalolo@gmail.com>
2026-03-20 15:56:30 +0800

    Add idle listener to fix badge not displaying after Chrome restart

--- background.js
+++ background.js
@@ -75,6 +75,18 @@ chrome.action.onClicked.addListener(() => {
   fetchXMRPrice(true);
 });
 
+chrome.runtime.onStartup.addListener(() => {
+  console.log('Browser started, initializing XMR price fetch');
+  fetchXMRPrice();
+});
+
+chrome.idle.onStateChanged.addListener((state) => {
+  if (state === 'active') {
+    console.log('System woke from idle, refreshing XMR price');
+    fetchXMRPrice();
+  }
+});
+
 console.log('Extension loaded, initiating first fetch');
 fetchXMRPrice();
 
